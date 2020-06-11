da945e236ea4c8c2f1f9dc1388a3aa3b0d4c96df
AnumEssani <essanianum@gmail.com>
2020-06-11 23:31:11 +0000

    update

---
+++ build/precache-manifest.d3fb3bac999ea13575c85a633955f2df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fd647b3b75f92bb538ac9a436ea4edc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d57b8a8d52955dc552fe",
+    "url": "/static/css/main.05bcbe20.chunk.css"
+  },
+  {
+    "revision": "6f83e4a9c3b33cb312dc",
+    "url": "/static/js/2.6eadf940.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6eadf940.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d57b8a8d52955dc552fe",
+    "url": "/static/js/main.f303e601.chunk.js"
+  },
+  {
+    "revision": "6b0269194706332ebee9",
+    "url": "/static/js/runtime-main.3070c762.js"
+  }
+]);
